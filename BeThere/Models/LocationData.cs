a77c6ba0bc05c4a8a4ee5cc30214338675e3131d
koistrach <koistrach@salesforce.com>
2023-09-09 17:21:07 +0300

    t

--- BeThere/Models/LocationData.cs
+++ BeThere/Models/LocationData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,21 +9,27 @@ namespace BeThere.Models
 {
     public class LocationData
     {
+
         public LocationData(double i_Latitude, double i_Longitude)
         {
             this.Latitude = i_Latitude;
             this.Longitude = i_Longitude;
+            InitializeCity();
         }
 
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public string City { get; set; }
 
+        private async void InitializeCity()
+        {
+            City = await GetLocationCity();
+        }
 
         public async Task<string> GetLocationAdress()
         {
             try
             {
-
                 string locationAdress = null;
                 var placemarks = await Geocoding.GetPlacemarksAsync(Latitude, Longitude);
 
@@ -33,6 +40,27 @@ namespace BeThere.Models
                 }
 
                 return locationAdress;
+            }
+            catch (Exception ex)
+            {
+                string message = ex.Message;
+                return null;
+            }
+        }
+
+        public async Task<string> GetLocationCity()
+        {
+            try
+            {
+                string locationCity = null;
+                var placemarks = await Geocoding.GetPlacemarksAsync(Latitude, Longitude);
+                if (placemarks != null && placemarks.Any())
+                {
+                    var placemark = placemarks.FirstOrDefault();
+                    locationCity = placemark.Locality;
+                }
+
+                return locationCity;
             }
             catch (Exception ex)
             {
